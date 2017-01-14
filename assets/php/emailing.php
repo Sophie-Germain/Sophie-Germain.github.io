6c68c79ac333c42eab39e67ede64963b5f64e8d1
NormaVTH <nvth28@gmail.com>
2017-01-14 15:44:26 -0600

    emailing

---
+++ assets/php/emailing.php
@@ -0,0 +1,13 @@
+<?php
+$to      = 'nvth_28@hotmail.com';
+$sender  = $_POST['sender'];
+$subject = $_POST['subject'];
+$message = "You have received a new message from ".$_POST['name'].":"."\r\n\n".
+			$_POST['comment'];
+$headers = 'From: '.$sender."\r\n".
+    'Reply-To: '.$sender."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+header("Location: ./index.html");
+?> 

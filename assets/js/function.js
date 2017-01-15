34cd74d16c6d75bd01b414689681ad823d4fc4b7
NormaVTH <nvth28@gmail.com>
2017-01-14 18:43:09 -0600

    emailing

---
+++ assets/js/function.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    $('.contact-btn[value="Send"]').on('click',function(){
+    	console.log("predsione");
+    	//alert("Your message has been sent!");
+    });
+});
